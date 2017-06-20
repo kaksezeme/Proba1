7eaf3503105c82ebcdf6143398dce2f5ee48b6ff
amcuric <acuric@foi.hr>
2017-06-20 19:58:13 +0200

    Poroba

---
+++ WindowsFormsApp1/WindowsFormsApp1/Klasa1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Klasa1
+    {
+        public static string DohvatiNekiString()
+        {
+            return "Neki string1";
+        }
+    }
+}
